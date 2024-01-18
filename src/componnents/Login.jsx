68ab5c05b5654dad564c1b96d981fb740aafea5e
Ibnlaouale <dinilaouale@gmail.com>
2024-01-18 02:01:27 +0100

    integration de boostrap et gestion de notif

--- src/componnents/Login.jsx
+++ src/componnents/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import logo from '../logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 function Login() {
     const [fullName, setFullName] = useState('');
     const [password, setPassword] = useState('');
@@ -26,28 +27,28 @@ function Login() {
         const email = formData.get('email');
         // Vérifier si l'utilisateur existe dans le localStorage
         const userExists = tableUsers.some(user => user.nom === fullName && user.motDePass === password);
-// ============ commentaire gestion des erreurs ==================
+        // ============ commentaire gestion des erreurs ==================
         if (fullName === '' || password === '' || email === '') {
-            toast.error('Veuillez renseigner tous les champs.', {theme: "colored"});
+            toast.error('Veuillez renseigner tous les champs.', { theme: "colored" });
         } else if (userExists) {
-            toast.info('L\'utilisateur existe déjà.', {theme: "colored"});
+            toast.info('L\'utilisateur existe déjà.', { theme: "colored" });
         } else {
             // Ajouter l'utilisateur s'il n'existe pas
-            const user = { nom: fullName, motDePass: password,mail: email };
+            const user = { nom: fullName, motDePass: password, mail: email };
             tableUsers.push(user);
-            toast.success('Utilisateur ajouté avec succès', {theme: "colored"});
+            toast.success('Utilisateur ajouté avec succès', { theme: "colored" });
             localStorage.setItem('tableUsers', JSON.stringify(tableUsers));
             console.log(tableUsers);
         }
-        const reset = ()=>{
-        setFullName('');
-        setPassword('');
-        setEmail('');
-    }
-    reset();
+        const reset = () => {
+            setFullName('');
+            setPassword('');
+            setEmail('');
+        }
+        reset();
         // Vous pouvez maintenant utiliser ces valeurs comme bon vous semble, par exemple, les comparer avec votre tableau d'utilisateurs.
     }
-// =================================================================
+    // =================================================================
     const handleFullNameChange = (e) => {
         setFullName(e.target.value);
     }
@@ -55,24 +56,27 @@ function Login() {
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     }
-    const handlMailChange = (e)=>{
+    const handlMailChange = (e) => {
         setEmail(e.target.value)
     }
 
     return (
-        <div>
+        <div className="container">
             <ToastContainer position="top-center"></ToastContainer>
-            <div className="divimg" style={{margin: 'auto'}}>
-            <img src={logo} className="App-logo" alt="logo" />
+            <div className="divimg" style={{ margin: 'auto' }}>
+                <img src={logo} className="App-logo" alt="logo" />
             </div>
-            <form onSubmit={connect}>
-                <label>Nom:</label>
-                <input type="text" placeholder="Nom" name="fullName" value={fullName} onChange={handleFullNameChange} />
-                <label>Password:</label>
-                <input type="password" placeholder="Mot de passe" name="password" value={password} onChange={handlePasswordChange} />
-                <label>Email</label>
-                <input type="email" placeholder="Mail" name="email" value={email} onChange={handlMailChange} />
-                <input className="btnC" type="submit" value={'enregistrer'} />
+            <form onSubmit={connect} >
+
+                    <label for="fullName" class="form-label">Nom:</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nom" name="fullName" value={fullName} onChange={handleFullNameChange} />
+                    <label for="fullName" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Mail" name="email" value={email} onChange={handlMailChange} />
+                    <label for="fullName" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Mot de passe" name="password" value={password} onChange={handlePasswordChange} />
+                    <input type="submit" class="form-control" id="exampleFormControlInput1" value={'Enregistrer'} className="mt-3 w-100 btnC btn text-white fw-bold fs-5"/>
+
+
             </form>
         </div>
     );
